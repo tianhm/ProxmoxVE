3b3a5e7d29822e2905c3302f9901a8c64ac359af
CanbiZ (MickLesk) <47820557+MickLesk@users.noreply.github.com>
2026-08-21 08:05:08 +0200

    bookorbit: bump default RAM to prevent tsc OOM segfault during nest build (#16649)

--- ct/bookorbit.sh
+++ ct/bookorbit.sh
@@ -8,7 +8,7 @@ source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxV
 APP="BookOrbit"
 var_tags="${var_tags:-books;library;reading}"
 var_cpu="${var_cpu:-2}"
-var_ram="${var_ram:-2048}"
+var_ram="${var_ram:-4096}"
 var_disk="${var_disk:-10}"
 var_os="${var_os:-debian}"
 var_version="${var_version:-13}"
