566c203a342dbfcaa2e474728503378d9e937151
CanbiZ (MickLesk) <47820557+MickLesk@users.noreply.github.com>
2026-08-24 13:40:43 +0200

    Gitea: fix git-over-SSH auth, group-writable home dir tripped sshd StrictModes (#16710)
    * gitea: fix git-over-SSH auth, group-writable home dir tripped sshd StrictModes
    
    * Update gitea-install.sh

--- install/gitea-install.sh
+++ install/gitea-install.sh
@@ -30,7 +30,7 @@ setup_deb_based() {
   chown -R gitea:gitea /var/lib/gitea/
   chmod -R 750 /var/lib/gitea/
   chown root:gitea /etc/gitea
-  chmod 770 /etc/gitea
+  chmod 750 /etc/gitea
   sudo -u gitea ln -s /var/lib/gitea/data/.ssh/ /etc/gitea/.ssh
   msg_ok "Configured Gitea"
 
