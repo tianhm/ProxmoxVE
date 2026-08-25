1f109e57141b1b3880d2d5569cb7d58940c27b0d
MickLesk <47820557+MickLesk@users.noreply.github.com>
2026-08-25 16:04:44 +0200

    gitea: fix ordering regression, chmod 750 before ln -s broke symlink creation

--- install/gitea-install.sh
+++ install/gitea-install.sh
@@ -30,8 +30,9 @@ setup_deb_based() {
   chown -R gitea:gitea /var/lib/gitea/
   chmod -R 750 /var/lib/gitea/
   chown root:gitea /etc/gitea
-  chmod 750 /etc/gitea
+  chmod 770 /etc/gitea
   sudo -u gitea ln -s /var/lib/gitea/data/.ssh/ /etc/gitea/.ssh
+  chmod 750 /etc/gitea
   msg_ok "Configured Gitea"
 
   msg_info "Creating Service"
